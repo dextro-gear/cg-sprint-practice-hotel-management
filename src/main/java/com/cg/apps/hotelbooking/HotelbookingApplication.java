38dafe3b46b335aee99d6fa768672efbc6a82dd8
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-16 12:58:47 +0530

    Add Console UI for room management system

--- src/main/java/com/cg/apps/hotelbooking/HotelbookingApplication.java
+++ src/main/java/com/cg/apps/hotelbooking/HotelbookingApplication.java
@@ -1,13 +1,19 @@
 package com.cg.apps.hotelbooking;
 
+import com.cg.apps.hotelbooking.roomms.ui.RoomConsoleUI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HotelbookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelbookingApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(HotelbookingApplication.class, args);
+
+		RoomConsoleUI roomUI = context.getBean(RoomConsoleUI.class);
+		roomUI.start();
 	}
 
 }
---
+++ src/main/java/com/cg/apps/hotelbooking/roomms/ui/RoomConsoleUI.java
@@ -0,0 +1,23 @@
+package com.cg.apps.hotelbooking.roomms.ui;
+
+import com.cg.apps.hotelbooking.roomms.services.RoomServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoomConsoleUI {
+
+    @Autowired
+    RoomServiceImpl roomService;
+
+    public void start(){
+
+        System.out.println(roomService.addRoom(12, 1, 12, 2000).toString());
+        System.out.println(roomService.addRoom(12, 1, 13, 1000).toString());
+        System.out.println(roomService.addRoom(12, 1, 14, 2500).toString());
+        System.out.println(roomService.addRoom(12, 1, 15, 3000).toString());
+        System.out.println(roomService.addRoom(12, 1, 16, 2450).toString());
+
+    }
+
+}
