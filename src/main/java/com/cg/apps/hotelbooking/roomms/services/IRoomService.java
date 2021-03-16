7db5e681802b1a90e0f104888049913715334a0e
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-16 12:45:28 +0530

    Update room add method to accept cost

--- src/main/java/com/cg/apps/hotelbooking/roomms/services/IRoomService.java
+++ src/main/java/com/cg/apps/hotelbooking/roomms/services/IRoomService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface IRoomService {
 
-    Room addRoom(long hotelID, int floorNo, int roomNo);
+    Room addRoom(long hotelID, int floorNo, int roomNo, double cost);
     Room findByID(long roomID);
     Room retrieveRoom(int floorNo, int roomNo);
     List<Room> retrieveAllRoomsInHotel(long hotelID);
--- src/main/java/com/cg/apps/hotelbooking/roomms/services/RoomServiceImpl.java
+++ src/main/java/com/cg/apps/hotelbooking/roomms/services/RoomServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cg.apps.hotelbooking.roomms.services;
 
+import com.cg.apps.hotelbooking.roomms.dao.RoomDAOImpl;
 import com.cg.apps.hotelbooking.roomms.entities.Room;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -10,24 +12,27 @@ import java.util.List;
 @Service
 public class RoomServiceImpl implements IRoomService{
 
+    @Autowired
+    RoomDAOImpl roomDAO;
+
     @Override
-    public Room addRoom(long hotelID, int floorNo, int roomNo) {
-        return null;
+    public Room addRoom(long hotelID, int floorNo, int roomNo, double cost) {
+        return roomDAO.addRoom(hotelID, floorNo, roomNo, cost);
     }
 
     @Override
     public Room findByID(long roomID) {
-        return null;
+        return roomDAO.findById(roomID);
     }
 
     @Override
     public Room retrieveRoom(int floorNo, int roomNo) {
-        return null;
+        return roomDAO.retrieveRoom(floorNo, roomNo);
     }
 
     @Override
     public List<Room> retrieveAllRoomsInHotel(long hotelID) {
-        return null;
+        return roomDAO.retrieveAllRooms(hotelID);
     }
 
     @Override
