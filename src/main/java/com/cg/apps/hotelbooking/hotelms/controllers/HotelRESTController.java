33cbbc9890855547926f12f9c35f35acf8b09c7e
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-16 13:42:19 +0530

    Add REST controllers for roomms and hotelms

---
+++ src/main/java/com/cg/apps/hotelbooking/hotelms/controllers/HotelRESTController.java
@@ -0,0 +1,7 @@
+package com.cg.apps.hotelbooking.hotelms.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HotelRESTController {
+}
---
+++ src/main/java/com/cg/apps/hotelbooking/roomms/controllers/RoomRESTController.java
@@ -0,0 +1,7 @@
+package com.cg.apps.hotelbooking.roomms.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoomRESTController {
+}
