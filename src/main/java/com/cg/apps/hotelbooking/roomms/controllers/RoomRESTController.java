ed20f8652cb9a6d9396b2aba77e37b8a8cfaeb66
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-17 09:53:30 +0530

    Open base room endpoints

--- src/main/java/com/cg/apps/hotelbooking/roomms/controllers/RoomRESTController.java
+++ src/main/java/com/cg/apps/hotelbooking/roomms/controllers/RoomRESTController.java
@@ -1,7 +1,13 @@
 package com.cg.apps.hotelbooking.roomms.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/room")
 @RestController
 public class RoomRESTController {
+
+
+
 }
