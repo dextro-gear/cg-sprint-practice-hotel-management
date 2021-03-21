6f52be7610d03f51109983ba222295b8a740ee7a
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-03-21 10:32:48 +0530

    Open endpoint to add and find hotels, add new rooms to hotel

--- src/main/java/com/cg/apps/hotelbooking/hotelms/controllers/HotelRESTController.java
+++ src/main/java/com/cg/apps/hotelbooking/hotelms/controllers/HotelRESTController.java
@@ -1,31 +1,42 @@
 package com.cg.apps.hotelbooking.hotelms.controllers;
 
-import com.cg.apps.hotelbooking.hotelms.dto.AddNewHotelRequest;
+import com.cg.apps.hotelbooking.hotelms.dto.*;
 import com.cg.apps.hotelbooking.hotelms.service.HotelServiceImpl;
+import com.cg.apps.hotelbooking.hotelms.utilities.HotelUtilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/hotel")
+@RestController
+@RequestMapping("/hotel")
 public class HotelRESTController {
 
     @Autowired
     HotelServiceImpl hotelService;
 
-    /*
-     * Add Hotel.
-     * Find hotel by ID.
-     * Find all guests living in a hotel.
-     * Find all guests checked out today.
+    @Autowired
+    HotelUtilities hotelUtil;
+
+    /* TODO
+     *  - Add Hotel.
+     *  - Add Hotel with rooms.
+     *  - Find hotel by ID.
+     *  - Find all guests living in a hotel.
+     *  - Find all guests checked out today.
      */
 
     @PostMapping("/add")
-    public String addNewHotel(@RequestBody AddNewHotelRequest request){
-        return "Name: " + request.getName() + " Address: " + request.getAddress();
+    public IHotelJSON addNewHotelWithoutRooms(@RequestBody SimpleAddNewHotelRequest request){
+        return hotelUtil.toJSON(hotelService.addHotel(request.getName(), request.getAddress()));
     }
 
     @GetMapping("/find/byID/{id}")
-    public String finHotelByID(@PathVariable("id") int id){
-        return "Hotel #" + id;
+    public IHotelJSON findHotelByID(@PathVariable("id") long id){
+        return hotelUtil.toJSON(hotelService.findById(id));
+    }
+
+    @PostMapping("/addRoomsToHotel")
+    public IHotelJSON addRoomsToHotel(@RequestBody AddRoomsToHotelRequest request){
+        return hotelUtil.toJSON(hotelService.addRoomsToHotel(request.getHotelID(), request.getRooms()));
     }
 
     @GetMapping("/find/guests/{id}")
